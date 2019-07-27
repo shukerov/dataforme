85693a3f98cbbff2cba1ba5607ecdc6753d444e1
Stoyan Shukerov <stoichik@gmail.com>
2019-07-26 19:51:48 -0700

    Drag and drop works on filepicker.
    Added some animations, to the filepicker icon. Felt cute, might strip it
    out later.

--- src/js/components/filePicker.js
+++ src/js/components/filePicker.js
@@ -10,34 +10,63 @@ export class FilePicker {
     this.self = document.createElement('div');
     this.self.classList.add('filepicker-wrapper');
     this.self.innerHTML = `\
-          <label for='file-upload' class='file-upload-field'>\
-              <div id='report-upload-icon'></div>
-              Drag and drop or pick file here\
+          <label for='file-upload' id='visual' class='file-upload-field'>\
+              <div id='report-upload-icon'></div>\
+              <p class='fp-text fp-strong'>Select Files</p>\
+              <p class='fp-text'>or Drag and Drop</p>\
           </label>\
           <input id='file-upload' type='file'/>\
           `;
 
     // get icon from the above html
-    let iconContainer = this.self.children[0].children[0];
+    this.selfVisual = this.self.children[0];
+    let iconContainer = this.selfVisual.children[0];
     iconContainer.innerHTML = iupload;
     parent.appendChild(this.self);
-    // return parent ? (parent.appendChild(this.self)) : (this.self);
   }
 
   onUpload(callback) {
     // to attach an action 
     this.input = this.self.children[1];
-    this.input.onchange = this.getFileSafe.bind(this, callback); 
+    let fileSelectHandler = this.getFileSafe.bind(this, callback); 
+
+    this.input.addEventListener('change', fileSelectHandler, false); 
+    this.selfVisual.addEventListener('mouseenter', this.fileDragHover, false);
+    this.selfVisual.addEventListener('mouseleave', this.fileDragHover, false);
+    this.selfVisual.addEventListener('drop', fileSelectHandler, false);
+    this.selfVisual.addEventListener('dragover', this.fileDragHover, false);
+    this.selfVisual.addEventListener('dragleave', this.fileDragHover, false);
   }
 
-  getFileSafe(callback) {
-    // file isn't a zip file. TODO should also check for file size
+  //TODO: maybe a helper if used elsewhere
+  // callback and event
+  getFileSafe(callback, e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var files = e.target.files || e.dataTransfer.files;
+    // file isn't a zip file.
+    // TODO should also check for file size
     // once the limitations of the script are more clear
-    if (this.input.files[0].name.endsWith('.zip')) {
-      callback(this.input.files[0]);
+    if (files.length == 1 && files[0].name.endsWith('.zip')) {
+      callback(files[0]);
     }
     else {
       alert('Please upload a zip file');
+    }
+  }
+
+  // changes style on drag events in the file picker
+  fileDragHover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    if (e.target.id == 'visual') {
+      if (e.type == 'dragover' || e.type == 'mouseenter') {
+        e.target.classList.add('file-upload-field-hover');
+      }
+      else {
+        e.target.classList.remove('file-upload-field-hover');
+      }
     }
   }
 }
