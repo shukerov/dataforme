0551a0602df7b9fba3bf40e8c57d48cf6090f664
Stoyan Shukerov <stoichik@gmail.com>
2019-10-14 18:50:03 -0700

    Spotify page content update... still needs a lot of work

--- src/page-spotify/main.js
+++ src/page-spotify/main.js
@@ -1,9 +1,1 @@
-import '../styles/main.scss';
-import { ClockChart } from '../js/clock-chart/clock_graph.js';
-
-var fakeData = [4648, 3840, 2475, 1201, 844, 873, 1386, 1952, 5032, 7815,
-  9034, 3681, 4076, 4677, 6684, 6501, 5244, 3797, 3344, 4265, 4054, 4352,
-  4538, 4778];
-
-var container = document.getElementById("svg-testing");
-var graph = new ClockChart(fakeData, 600, container, {addLegend: true});
+import '../styles/spotify.scss';
